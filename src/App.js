3e09ed42bd15b3d280cf80d54b91db722dec67d0
mafb <marcofbraga@gmail.com>
2026-02-09 18:36:18 -0300

    Primeiro site

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>Ávida Cervejaria</h1>
+        <p>Um brinde à vida!</p>
+        <a href="https://www.instagram.com/bebaavida" target="_blank" rel="noopener noreferrer" className="instagram-link">
+          Siga @bebaavida no Instagram
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
