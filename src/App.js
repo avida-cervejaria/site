867fd28334e06465ab10b77caadc24e02cf418ad
mafb <marcofbraga@gmail.com>
2026-02-10 18:03:44 -0300

    Adicionando CNPJ no site para verificaçao da Meta

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ function App() {
       <div className="content">
         <h1>Ávida Cervejaria</h1>
         <p>Um brinde à vida!</p>
+
         <a href="https://www.instagram.com/bebaavida" target="_blank" rel="noopener noreferrer" className="instagram-link">
           Siga @bebaavida no Instagram
         </a>
+
+        <footer className="footer">
+          SETA INDUSTRIA DE BEBIDAS LTDA | CNPJ: 63.943.201/0001-42
+        </footer>
       </div>
     </div>
   );
